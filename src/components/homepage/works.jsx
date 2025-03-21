e663d5222086506cf633ec5baf884bd8c7a9ecaf
Ali <83744482+aalligithub@users.noreply.github.com>
2025-03-21 17:58:29 +0330

    work added

--- src/components/homepage/works.jsx
+++ src/components/homepage/works.jsx
@@ -4,6 +4,11 @@ import { faBriefcase } from "@fortawesome/free-solid-svg-icons";
 import Card from "../common/card";
 
 import "./styles/works.css";
+import INFO from "../../data/user";
+
+import jahadLogo from "../../data/jahadLogo.jpg"
+import lionLogo from "../../data/lionLogo.jpg"
+import tahlilgaran from "../../data/tahlilgaran.jpg"
 
 const Works = () => {
 	return (
@@ -13,30 +18,53 @@ const Works = () => {
 				title="Work"
 				body={
 					<div className="works-body">
+
 						<div className="work">
 							<img
-								src="./facebook.png"
-								alt="facebook"
+								src={tahlilgaran}
 								className="work-image"
 							/>
-							<div className="work-title">Facebook</div>
+							<div className="work-title">Tahlilgaran System Zagros</div>
 							<div className="work-subtitle">
 								Software Engineer
 							</div>
-							<div className="work-duration">2019 - Present</div>
+							<div className="work-duration">December 2024 - Now</div>
 						</div>
 
 						<div className="work">
 							<img
-								src="./twitter.png"
-								alt="twitter"
+								src="https://img.icons8.com/?size=100&id=64238&format=png&color=000000"
 								className="work-image"
 							/>
-							<div className="work-title">Twitter</div>
+							<div className="work-title">Freelance Software Developer</div>
 							<div className="work-subtitle">
-								Software Engineer
+								Software Developer
 							</div>
-							<div className="work-duration">2019 - Present</div>
+							<div className="work-duration">June 2018 - Now</div>
+						</div>
+
+						<div className="work">
+							<img
+								src={lionLogo}
+								className="work-image"
+							/>
+							<div className="work-title">Lion Computer</div>
+							<div className="work-subtitle">
+								Technical Support Engineer
+							</div>
+							<div className="work-duration">2019 - 2021</div>
+						</div>
+
+						<div className="work">
+							<img
+								src={jahadLogo}
+								className="work-image"
+							/>
+							<div className="work-title">Jahad Institude</div>
+							<div className="work-subtitle">
+								Programming tutor
+							</div>
+							<div className="work-duration">June 2018 - August 2018</div>
 						</div>
 					</div>
 				}
