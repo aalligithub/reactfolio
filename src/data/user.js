e59c2adf06aafbee6856888f74f1f8e10cd89384
Ali <83744482+aalligithub@users.noreply.github.com>
2025-03-21 12:35:21 +0330

    small personalization

--- src/data/user.js
+++ src/data/user.js
@@ -1,28 +1,28 @@
 const INFO = {
 	main: {
-		title: "Reactfolio by truethari",
-		name: "Tharindu N.",
-		email: "mail@tharindu.dev",
-		logo: "../logo.png",
+		title: "Ali Karimpour",
+		name: "Ali Karimpour",
+		email: "mynameisaalli@gmail.com",
+		logo: "../fdg.jpg",
 	},
 
 	socials: {
 		twitter: "https://twitter.com/",
-		github: "https://github.com/",
+		github: "https://github.com/aalligithub",
 		linkedin: "https://linkedin.com/",
-		instagram: "https://instagram.com/",
-		stackoverflow: "https://stackoverflow.com/",
+		instagram: "https://instagram.com/aalli_k",
+		stackoverflow: "https://stackoverflow.com/users/11687785/ali-karimpour?tab=profile",
 		facebook: "https://facebook.com/",
 	},
 
 	homepage: {
-		title: "Full-stack web and mobile app developer, and amateur astronaut.",
+		title: "Software Engineer | Backend Developer | Database Administrator",
 		description:
-			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
-	},
+		  "I am a versatile software engineer specializing in backend development, database design, and large-scale enterprise software. With experience in cutting-edge technologies like LLMs and a passion for solving complex problems, I deliver robust and scalable solutions. If it's software-related, I can do it—and I can do it well.",
+	  },
 
 	about: {
-		title: "I’m Tharindu N. I live in Sri Lanka, where I design the future.",
+		title: "I’m Ali Karimpour I live in Sri Lanka, where I design the future.",
 		description:
 			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
 	},
